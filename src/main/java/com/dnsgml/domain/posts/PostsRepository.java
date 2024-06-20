4bb859982506d9a55d3c1f0b98c0ff707bc3ba46
dnsgml <dnsgml7@gmail.com>
2024-06-20 14:17:26 +0900

    Homework #4

--- src/main/java/com/dnsgml/domain/posts/PostsRepository.java
+++ src/main/java/com/dnsgml/domain/posts/PostsRepository.java
@@ -1,9 +1,15 @@
 package com.dnsgml.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 
-public interface PostsRepository extends JpaRepository<Posts, Long> {
+public interface PostsRepository extends JpaRepository<Posts,Long> {
 
+    @Query(value = "SELECT p.* FROM posts p ORDER BY p.id DESC", nativeQuery = true)
+    List<Posts> findAllDesc();
+
+    List<Posts> findAllByOrderByIdDesc();
 }
 
--- src/main/java/com/dnsgml/service/posts/PostsService.java
+++ src/main/java/com/dnsgml/service/posts/PostsService.java
@@ -2,11 +2,15 @@ package com.dnsgml.service.posts;
 
 import com.dnsgml.domain.posts.Posts;
 import com.dnsgml.domain.posts.PostsRepository;
+import com.dnsgml.web.dto.PostsListResponseDto;
 import com.dnsgml.web.dto.PostsResponseDto;
 import com.dnsgml.web.dto.PostsSaveRequestDto;
-import jakarta.transaction.Transactional;
+import org.springframework.transaction.annotation.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -34,4 +38,23 @@ public class PostsService {
                 IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
         return new PostsResponseDto(entity);
     }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+//        return postsRepository.findAllDesc().stream()
+//                .map(PostsListResponseDto::new)
+//                .collect(Collectors.toList());
+
+        return postsRepository.findAllByOrderByIdDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete (Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id ));
+        postsRepository.delete(posts); // ①
+    }
+
 }
